b6a7f7f0294523b8ca094cda47f1a9655eae1a1e
alvinjo <alvin.joseph0@gmail.com>
2018-10-30 10:52:22 +0000

    account and service added

---
+++ src/Account.java
@@ -0,0 +1,39 @@
+public class Account {
+
+
+    private String firstName;
+    private String lastName;
+    private String accountNumber;
+
+
+    public Account(String first, String last, String acc){
+        this.firstName = first;
+        this.lastName = last;
+        this.accountNumber = acc;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getAccountNumber() {
+        return accountNumber;
+    }
+
+    public void setAccountNumber(String accountNumber) {
+        this.accountNumber = accountNumber;
+    }
+
+    public String getFirstName() {
+
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+}
---
+++ src/Service.java
@@ -0,0 +1,28 @@
+import java.util.HashMap;
+
+public class Service {
+
+
+    HashMap<String, Account> accountMap;
+
+    public Service(){
+        accountMap = new HashMap<>();
+    }
+
+    public Account getAccount(int accNum){
+        return accountMap.get(accNum);
+    }
+
+    public void putAccount(String accNum, Account account){
+        accountMap.put(account.getAccountNumber(), account);
+    }
+
+    public void addAndPutAccount(String first, String last, String acc){
+        accountMap.put(acc, new Account(first, last, acc));
+    }
+
+    public void removeAccount(int accNum){
+        accountMap.remove(accNum);
+    }
+
+}
