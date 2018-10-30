7fec7c65500303ceec931fc88f92195c1559ee65
alvinjo <alvin.joseph0@gmail.com>
2018-10-30 11:06:51 +0000

    refactored

---
+++ src/Accounts/App.java
@@ -0,0 +1,14 @@
+package Accounts;
+
+public class App {
+
+
+    public static void main(String[] args) {
+        App test = new App();
+    }
+
+    public App(){
+        System.out.println("Hello worlds");
+    }
+
+}
