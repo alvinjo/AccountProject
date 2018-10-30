63532d2d526b6bfacd2f604158131137a37ee2d3
alvinjo <alvin.joseph0@gmail.com>
2018-10-30 15:22:04 +0000

    fixed code smells

--- src/main/java/accounts/AccountToJSON.java
+++ src/main/java/accounts/AccountToJSON.java
@@ -3,18 +3,17 @@ package accounts;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
-import java.util.HashMap;
 import java.util.Map;
 
 public class AccountToJSON {
 
     ObjectMapper mapper = new ObjectMapper();
-    static Map<String,Account> accountMap;
+    Map<String,Account> accountMap;
 
     public AccountToJSON(Map<String,Account> map){
         this.accountMap = map;
     }
-    
+
 
     public String toJSON(){
         String jsonObj="";
