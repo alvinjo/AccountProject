71512a866ba38aaeef4f29e8cc5dd7a43da41c6d
alvinjo <alvin.joseph0@gmail.com>
2018-10-30 11:09:43 +0000

    refactored againn

--- src/accounts/App.java
+++ src/accounts/App.java
@@ -1,13 +1,18 @@
-package Accounts;
+package accounts;
 
 public class App {
 
 
     public static void main(String[] args) {
         App test = new App();
+        test.print();
     }
 
     public App(){
+
+    }
+
+    private void print(){
         System.out.println("Hello worlds");
     }
 
